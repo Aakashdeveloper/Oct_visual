8583716b986737583f802b55869063e5e87f80fd
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-10-30 07:19:03 +0530

    a

---
+++ javascript/ten.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName("container")
+HTMLCollection(4) [div.container, div.container, div.content.container, div.container]
+document.getElementsByClassName("container")[0].innerText
+"	
+Connected to GoToMeeting
+Questions about joining? Get support"
+document.getElementsByClassName("container")[1].innerText
+"Schedule a session right from your calendar."
+document.getElementsByClassName("container")[1].style.css.color="red"
+
+document.getElementsByClassName("container")[1].style.color="red"
+"red"
+document.getElementById('h1')
+null
+document.getElementById('h2')
+null
+document.getElementById('p')
+null
+document.getElementByTagName('li')
+document.getElementsByTagName('li')
+HTMLCollection(3) [li, li, li]
+document.getElementsByTagName('li')[0].innerText
+"Add GoToMeeting to your event with one click."
+document.getElementsByTagName('li')[0].innerText="hi"
+"hi"
+
